ca533019d305cb3ce0e7f728e92934d58979cf9e
neeraja410 <mudiredddy95@gmail.com>
2024-08-06 17:26:55 -0700

    added comments on mongodb example program

--- src/main/java/com/example/mongoDB/MongoDBExample.java
+++ src/main/java/com/example/mongoDB/MongoDBExample.java
@@ -14,6 +14,7 @@ import org.bson.Document;
 
 public class MongoDBExample {
     public static void main(String[] args) {
+       //we are in mongodb example program
         // Connect to MongoDB server
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 
